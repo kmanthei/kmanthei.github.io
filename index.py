3e0364bcbb345b73e0cb64a8e7a91971054cfbea
nyanford <143133478+nyanford@users.noreply.github.com>
2023-09-11 19:42:10 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,19 @@
+#Provides a way to provide the platform
 import os
 
+#This is the directory where the files are located 
 os.chdir('/Users/willh/Downloads/')
 
+# This is going to go through the file and find the lines that have the request
 try:
     total_requests = 0
     with open("logfile.txt", "r") as file:
         for line in file:
             total_requests += 1
+# If not found an error message will show
 except FileNotFoundError:
     print("Please check the path.")
-    
+
+#This will show the the amount it finds in the code    
 print("The total length is:", total_requests)
 print("The total length in the past six months is:", )
