a24bd514c941593c32942d06aa2c209993eb715f
WillHaney <106602318+WillHaney@users.noreply.github.com>
2023-09-18 16:40:57 -0500

    question7.py

--- question7.py
+++ question7.py
@@ -1,21 +1,26 @@
+# Import the following modules
 import os
 import re
 from datetime import datetime
 
+# Enter your current directory
 os.chdir('/Users/willh/Downloads/')
 
+# Edit according to your text file name
 input_log_file = 'logfile.txt'
 output_directory = 'logs_by_month'
 
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
+# Creates a dict
 month_files = {}
 
 with open(input_log_file, 'r') as input_file:
     for line in input_file:
         timestamp_match = re.search(r'\[(.*?)\]', line)
-        
+
+        # Find matches that match the below date format
         if timestamp_match:
             timestamp_str = timestamp_match.group(1)
             log_date = datetime.strptime(timestamp_str, '%d/%b/%Y:%H:%M:%S %z')
