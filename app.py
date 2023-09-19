3502e5ee8a565c844730868a3b29da5af6a520d2
edengracee <143214820+edengracee@users.noreply.github.com>
2023-09-19 17:07:21 -0500

    Group Project 5 (excludes slack endpoint)
    This works on my end for the first few endpoints, but I cannot figure out the Slack endpoint so I'd appreciate y'all's help on that one. Please test this out and let me know if it works. You must replace the "your_message/number" parts of the following URLs once you download the correct Python file to see the link work when launching in your browser.                http://localhost:4000/md5/your_string 
    http://localhost:4000/factorial/your_number
    http://localhost:4000/fibonacci/your_number
    http://localhost:4000/is-prime/your_number
    
    Need help with: 
    
    http://localhost:4000/slack-alert/your_message

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+import hashlib
+import math
+
+app = Flask(__name__)
+
+@app.route('/md5/<string:text>', methods=['GET'])
+def calculate_md5(text):
+    md5_hash = hashlib.md5(text.encode()).hexdigest()
+    return jsonify({"md5_hash": md5_hash})
+
+@app.route('/factorial/<int:num>', methods=['GET'])
+def calculate_factorial(num):
+    if num < 0:
+        return jsonify({"error": "Factorial is not defined for negative numbers"}), 400
+
+    try:
+        factorial_result = math.factorial(num)
+        return jsonify({"factorial": factorial_result})
+    except ValueError:
+        return jsonify({"error": "Invalid input. Please provide a valid non-negative integer."}), 400
+
+@app.route('/fibonacci/<int:num>', methods=['GET'])
+def calculate_fibonacci(num):
+    def fibonacci(n):
+        if n < 2:
+            return n
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
+    
+    fibonacci_sequence = [fibonacci(i) for i in range(num+1)]
+    return jsonify({"fibonacci_sequence": fibonacci_sequence})
+
+@app.route('/is-prime/<int:num>', methods=['GET'])
+def check_prime(num):
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+    
+    is_prime_result = is_prime(num)
+    return jsonify({"is_prime": is_prime_result})
+
+@app.route('/slack-alert/<string:message>', methods=['POST'])
+def send_slack_alert(message):
+    # Replace this with your code to send a Slack alert
+    # You can use a Slack API or a Python package for this purpose
+    # For simplicity, we'll just return a success message
+    return jsonify({"message": f"Slack alert sent: {message}"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000)
