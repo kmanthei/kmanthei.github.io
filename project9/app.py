1aae8dd2995a5829a972d3b9c918ec2a7c199a5b
nyanford <143133478+nyanford@users.noreply.github.com>
2023-12-11 11:01:08 -0600

    Add files via upload

---
+++ project9/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+import hashlib
+import math
+
+# String for mdd5
+app = Flask(__name__)
+
+@app.route('/md5/<string:text>', methods=['GET'])
+def calculate_md5(text):
+    md5_hash = hashlib.md5(text.encode()).hexdigest()
+    return jsonify({"md5_hash": md5_hash})
+
+#String for factorial
+@app.route('/factorial/<int:num>', methods=['GET'])
+def calculate_factorial(num):
+    if num < 0:
+        return jsonify({"error": "Factorial is not defined for negative numbers"}), 400
+
+    try:
+        factorial_result = math.factorial(num)
+        return jsonify({"factorial": factorial_result})
+    except ValueError:
+        return jsonify({"error": "Invalid input. Please provide a valid non-negative integer."}), 400
+
+#String for fibonacci
+@app.route('/fibonacci/<int:num>', methods=['GET'])
+def calculate_fibonacci(num):
+    def fibonacci(n):
+        if n < 2:
+            return n
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
+    
+    fibonacci_sequence = [fibonacci(i) for i in range(num+1)]
+    return jsonify({"fibonacci_sequence": fibonacci_sequence})
+
+#String for is-prime
+@app.route('/is-prime/<int:num>', methods=['GET'])
+def check_prime(num):
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+    
+    is_prime_result = is_prime(num)
+    return jsonify({"is_prime": is_prime_result})
+
+#String for slack alert
+@app.route('/slack-alert/<string:message>', methods=['POST'])
+def send_slack_alert(message):
+   
+    return jsonify({"message": f"Slack alert sent: {message}"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000)
